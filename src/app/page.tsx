f34ce61222807a7e05933ffbfd9126aa8064bee6
michaelsatumba <mjsatumba@gmail.com>
2024-12-19 15:47:23 -0800

    title

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -169,7 +169,7 @@ const Home: React.FC = () => {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Head>
-        <title>P2P Slider</title>
+        <title>Rebet Slider</title>
         <meta name="description" content="P2P Slider Component" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
